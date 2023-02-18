21d7fd7ca2d872d9ee97a407b1382023c2d27326
Vetri2646 <105487992+Vetri2646@users.noreply.github.com>
2023-02-18 15:14:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load",function(){
+    document.getElementById("popup").style.display="block";
+});
+function close_ad(){
+    document.getElementById("popup").style.display="none";
+}
